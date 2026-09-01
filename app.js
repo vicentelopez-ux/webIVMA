e4a8c503ef11f4fb7419f5d71a8e78273839d373
diegovalenzuelaleon41-svg <diegovalenzuelaleon41@gmail.com>
2026-09-01 13:10:31 -0400

    EJS

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require('express');
-
+const path = require('path');
 const app = express(); // App hereda todo lo que contenga express
 
 const PORT = 3000;
 
-// RUTAS
+// Instalar EJS con npm i ejs
+// CONFIGURAR EJS
+app.set('view engine', 'ejs');
 
+// ARCHIVOS ESTATICOS
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// RUTAS
 app.get('/', (req, res) => {
-  res.send('<h1>INDEX</h1>');
+  res.render('index'); // Carga la pagina ./views/index.ejs
 }); 
 
 app.get('/servicios', (req, res) => {
-  res.send('<h1>SERVICIOS</h1>');
+  res.render('servicios');
 }); 
 
 app.get('/nosotros', (req, res) => {
-  res.send('<h1>NOSOTROS</h1>');
+  res.render('nosotros');
 }); 
 
 app.get('/contacto', (req, res) => {
-  res.send('<h1>CONTACTO</h1>');
+  res.render('contacto');
 }); 
 
 // Colocamos el app en el puerto 3000
